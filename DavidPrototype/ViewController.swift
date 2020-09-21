ff88d3c8ee37d60e242af8eda9bb59e3e27df5a9
davidshin <hkluv1009@gmail.com>
2020-09-21 21:28:19 +0900

    Initial Commit

---
+++ DavidPrototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DavidPrototype
+//
+//  Created by David Shin on 2020/09/21.
+//  Copyright © 2020 David Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
